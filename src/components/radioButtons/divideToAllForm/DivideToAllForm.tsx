a22a3448c4dea487cd731d32db0594135bdd1877
ChikaEJ <ecd4gg@gmail.com>
2024-01-23 21:04:25 +0600

    Added all porps to divideToAll form

--- src/components/radioButtons/divideToAllForm/DivideToAllForm.tsx
+++ src/components/radioButtons/divideToAllForm/DivideToAllForm.tsx
@@ -1,23 +1,41 @@
 import styles from "./DivideToAll.module.css";
 interface IDivideToAllFormProps {
-    submitDivideToAllForm: () => void;
+    submitDivideToAllForm: (e: React.FormEvent<HTMLFormElement>) => void;
+    inputPersonsHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    inputSumOfOrderHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    inputTipsPercentHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    inputDeliveryHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    divideToAllInputValue: {
+        perosns: number, 
+        sumOfOrder: number, 
+        tipsPercent: number, 
+        delivery: number
+    };
 }
 
-const DivideToAllForm: React.FC<IDivideToAllFormProps> = ({ submitDivideToAllForm, }) => {
+const DivideToAllForm: React.FC<IDivideToAllFormProps> = (
+    { 
+        submitDivideToAllForm, 
+        inputPersonsHandler, 
+        inputSumOfOrderHandler, 
+        inputTipsPercentHandler, 
+        inputDeliveryHandler, 
+        divideToAllInputValue 
+    }) => {
     return (
         <div>
-            <form onSubmit={submitDivideToAllForm}>
+            <form onSubmit={(e) => submitDivideToAllForm(e)}>
                 <label>Человек:
-                    <input type="number" />
+                    <input type="number" onChange={(e) => inputPersonsHandler(e)} value={divideToAllInputValue.perosns} />
                 </label>
                 <label>Сумма заказа:
-                    <input type="number" />
+                    <input type="number" onChange={(e) => inputSumOfOrderHandler(e)} value={divideToAllInputValue.sumOfOrder} />
                 </label>
                 <label>Процент чаевых:
-                    <input type="number" />
+                    <input type="number" onChange={(e) => inputTipsPercentHandler(e)} value={divideToAllInputValue.tipsPercent} />
                 </label>
                 <label>Доставка:
-                    <input type="number" />
+                    <input type="number" onChange={(e) => inputDeliveryHandler(e)} value={divideToAllInputValue.delivery} />
                 </label>
 
                 <input type="button" value="Расчитать" />
