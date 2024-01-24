3a9cad73d233a8ce15392cd6a623c39105e8d3dd
ChikaEJ <ecd4gg@gmail.com>
2024-01-24 15:24:57 +0600

    empty order page is created

---
+++ src/containers/orderCoutnPage/OrderCoutnPage.tsx
@@ -0,0 +1,11 @@
+import styles from "./OrderCountPage.module.css"
+
+const OrderCountPage: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default OrderCountPage;
